fc7ed35e610798057ff56db89aef243b9bfef73b
LeoCode27 <101262535+LeoCode27@users.noreply.github.com>
2022-04-02 02:34:10 +0600

    Add files via upload

---
+++ Comment.java
@@ -0,0 +1,9 @@
+public class Print{
+	public static void main(String[] args){
+		//Single line comment
+		/*
+		Multi-line comment
+		*/
+		System.out.println("Print Java");
+	}
+}
---
+++ Variable.java
@@ -0,0 +1,10 @@
+public class Print{
+	public static void main(String[] args){
+		String s="String";
+		int i=3;
+		float f=2.435;
+		char c='c';
+		boolean b= true;
+		System.out.println("Print Java");
+	}
+}
